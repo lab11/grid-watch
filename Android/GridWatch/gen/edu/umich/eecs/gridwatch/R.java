af02a8cdd314d25214d32b005be1af63e074c251
Pat Pannuto <pat.pannuto@gmail.com>
2013-06-27 18:43:10 -0400

    Initial layout, charging status

--- Android/GridWatch/gen/edu/umich/eecs/gridwatch/R.java
+++ Android/GridWatch/gen/edu/umich/eecs/gridwatch/R.java
@@ -24,7 +24,17 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int accel_status=0x7f080007;
+        public static final int action_settings=0x7f08000a;
+        public static final int charger_status=0x7f080001;
+        public static final int current_location=0x7f080003;
+        public static final int force_accel_update=0x7f080009;
+        public static final int force_location_update=0x7f080005;
+        public static final int tableRow1=0x7f080000;
+        public static final int tableRow2=0x7f080002;
+        public static final int tableRow3=0x7f080004;
+        public static final int tableRow4=0x7f080006;
+        public static final int tableRow5=0x7f080008;
     }
     public static final class layout {
         public static final int activity_grid_watch=0x7f030000;
@@ -33,9 +43,18 @@ public final class R {
         public static final int grid_watch=0x7f070000;
     }
     public static final class string {
+        public static final int accel_status=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int charger_status=0x7f050003;
+        public static final int current_location=0x7f050004;
+        public static final int force_accel_update=0x7f050007;
+        public static final int force_location_update=0x7f050005;
+        public static final int power_ac=0x7f050008;
+        public static final int power_none=0x7f05000b;
+        public static final int power_usb=0x7f050009;
+        public static final int power_wireless=0x7f05000a;
+        public static final int title_text=0x7f050002;
     }
     public static final class style {
         /** 
--- Android/GridWatch/src/edu/umich/eecs/gridwatch/GridWatch.java
+++ Android/GridWatch/src/edu/umich/eecs/gridwatch/GridWatch.java
@@ -1,15 +1,41 @@
 package edu.umich.eecs.gridwatch;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class GridWatch extends Activity {
+	TextView mChargerStatus;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_grid_watch);
+		
+		mChargerStatus = (TextView) findViewById(R.id.charger_status);
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+		Intent batteryStatus = registerReceiver(null, ifilter);
+		
+		int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+		if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
+			mChargerStatus.setText(R.string.power_ac);
+		} else if (chargePlug == BatteryManager.BATTERY_PLUGGED_USB) {
+			mChargerStatus.setText(R.string.power_usb);
+		} else if (chargePlug == BatteryManager.BATTERY_PLUGGED_WIRELESS) {
+			mChargerStatus.setText(R.string.power_wireless);
+		} else {
+			mChargerStatus.setText(R.string.power_none);
+		}
 	}
 
 	@Override
