e8abb963f3904247b14dd67baf30861ac49c2d40
Pat Pannuto <pat.pannuto@gmail.com>
2013-06-27 20:12:04 -0400

    Add dock checking, error checking

--- Android/GridWatch/gen/edu/umich/eecs/gridwatch/R.java
+++ Android/GridWatch/gen/edu/umich/eecs/gridwatch/R.java
@@ -24,17 +24,19 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int accel_status=0x7f080007;
-        public static final int action_settings=0x7f08000a;
+        public static final int accel_status=0x7f080009;
+        public static final int action_settings=0x7f08000c;
         public static final int charger_status=0x7f080001;
-        public static final int current_location=0x7f080003;
-        public static final int force_accel_update=0x7f080009;
-        public static final int force_location_update=0x7f080005;
-        public static final int tableRow1=0x7f080000;
-        public static final int tableRow2=0x7f080002;
-        public static final int tableRow3=0x7f080004;
-        public static final int tableRow4=0x7f080006;
-        public static final int tableRow5=0x7f080008;
+        public static final int current_location=0x7f080005;
+        public static final int dock_status=0x7f080003;
+        public static final int force_accel_update=0x7f08000b;
+        public static final int force_location_update=0x7f080007;
+        public static final int tableRow0=0x7f080000;
+        public static final int tableRow1=0x7f080002;
+        public static final int tableRow2=0x7f080004;
+        public static final int tableRow3=0x7f080006;
+        public static final int tableRow4=0x7f080008;
+        public static final int tableRow5=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_grid_watch=0x7f030000;
@@ -43,17 +45,24 @@ public final class R {
         public static final int grid_watch=0x7f070000;
     }
     public static final class string {
-        public static final int accel_status=0x7f050006;
+        public static final int accel_status=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int charger_status=0x7f050003;
-        public static final int current_location=0x7f050004;
-        public static final int force_accel_update=0x7f050007;
-        public static final int force_location_update=0x7f050005;
-        public static final int power_ac=0x7f050008;
-        public static final int power_none=0x7f05000b;
-        public static final int power_usb=0x7f050009;
-        public static final int power_wireless=0x7f05000a;
+        public static final int current_location=0x7f050005;
+        public static final int dock_car=0x7f05000e;
+        public static final int dock_desk=0x7f05000f;
+        public static final int dock_err=0x7f050011;
+        public static final int dock_none=0x7f050010;
+        public static final int dock_status=0x7f050004;
+        public static final int dock_unsupported=0x7f050012;
+        public static final int force_accel_update=0x7f050008;
+        public static final int force_location_update=0x7f050006;
+        public static final int power_ac=0x7f050009;
+        public static final int power_err=0x7f05000d;
+        public static final int power_none=0x7f05000c;
+        public static final int power_usb=0x7f05000a;
+        public static final int power_wireless=0x7f05000b;
         public static final int title_text=0x7f050002;
     }
     public static final class style {
--- Android/GridWatch/src/edu/umich/eecs/gridwatch/GridWatch.java
+++ Android/GridWatch/src/edu/umich/eecs/gridwatch/GridWatch.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class GridWatch extends Activity {
 	TextView mChargerStatus;
+	TextView mDockStatus;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,14 +19,31 @@ public class GridWatch extends Activity {
 		setContentView(R.layout.activity_grid_watch);
 		
 		mChargerStatus = (TextView) findViewById(R.id.charger_status);
+		mDockStatus = (TextView) findViewById(R.id.dock_status);
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
-		
+
+		updateBattery();
+		updateDock();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.grid_watch, menu);
+		return true;
+	}
+
+	private void updateBattery() {
 		IntentFilter ifilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
 		Intent batteryStatus = registerReceiver(null, ifilter);
+		if (batteryStatus == null) {
+			mChargerStatus.setText(R.string.power_err);
+			return;
+		}
 		
 		int chargePlug = batteryStatus.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
 		if (chargePlug == BatteryManager.BATTERY_PLUGGED_AC) {
@@ -37,12 +56,33 @@ public class GridWatch extends Activity {
 			mChargerStatus.setText(R.string.power_none);
 		}
 	}
+	
+	private void updateDock() {
+		mDockStatus.setText(R.string.dock_unsupported);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.grid_watch, menu);
-		return true;
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+			IntentFilter ifilter = new IntentFilter(Intent.ACTION_DOCK_EVENT);
+			Intent dockStatus = registerReceiver(null, ifilter);
+			if (dockStatus == null) {
+				mDockStatus.setText(R.string.dock_err);
+				return;
+			}
+			
+			int dockState = dockStatus.getIntExtra(Intent.EXTRA_DOCK_STATE, -1);
+			
+			boolean isCar = dockState == Intent.EXTRA_DOCK_STATE_CAR;
+			boolean isDesk =
+					dockState == Intent.EXTRA_DOCK_STATE_DESK ||
+					dockState == Intent.EXTRA_DOCK_STATE_HE_DESK ||
+					dockState == Intent.EXTRA_DOCK_STATE_LE_DESK;
+			
+			if (isCar) {
+				mDockStatus.setText(R.string.dock_car);
+			} else if (isDesk) {
+				mDockStatus.setText(R.string.dock_desk);
+			} else {
+				mDockStatus.setText(R.string.dock_none);
+			}
+		}
 	}
-
 }
